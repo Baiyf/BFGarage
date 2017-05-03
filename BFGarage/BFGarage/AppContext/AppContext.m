7444003984ed3658538796b9002152659bb77add
baiyufei <baiyufei@autohome.com.cn>
2017-05-03 18:40:05 +0800

    修改公司名

--- BFGarage/BFGarage/AppContext/AppContext.m
+++ BFGarage/BFGarage/AppContext/AppContext.m
@@ -3,7 +3,7 @@
 //  BFGarage
 //
 //  Created by baiyufei on 2017/4/23.
-//  Copyright © 2017年 com.autohome. All rights reserved.
+//  Copyright © 2017年 com.zhouqinghua. All rights reserved.
 //
 
 #import "AppContext.h"

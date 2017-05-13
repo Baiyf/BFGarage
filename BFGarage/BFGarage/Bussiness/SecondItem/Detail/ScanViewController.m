7309eae7c21ae8a83ad8804d19d281f0a6791eee
baiyufei <baiyufei@autohome.com.cn>
2017-05-13 15:18:06 +0800

    证书添加，扫描添加

--- BFGarage/BFGarage/Bussiness/SecondItem/Detail/ScanViewController.m
+++ BFGarage/BFGarage/Bussiness/SecondItem/Detail/ScanViewController.m
@@ -3,7 +3,7 @@
 //  BFGarage
 //
 //  Created by baiyufei on 2017/4/23.
-//  Copyright © 2017年 com.zhouqinghua. All rights reserved.
+//  Copyright © 2017年 com.autohome. All rights reserved.
 //
 
 #import "ScanViewController.h"
@@ -38,18 +38,17 @@
     
     AVCaptureDeviceInput *captureInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
     
-    NSString *mediaType = AVMediaTypeVideo;
-    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:mediaType];
-    if(authStatus == AVAuthorizationStatusRestricted || authStatus == AVAuthorizationStatusDenied){
-        UIAlertView *alert =[[UIAlertView alloc]initWithTitle:@"提示" message:@"请在iPhone的“设置”-“隐私”-“相机”功能中" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-        [alert show];
-        return;
-    }
+//    NSString *mediaType = AVMediaTypeVideo;
+//    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:mediaType];
+//    if(authStatus == AVAuthorizationStatusRestricted || authStatus == AVAuthorizationStatusDenied){
+//        UIAlertView *alert =[[UIAlertView alloc]initWithTitle:@"提示" message:@"请在iPhone的“设置”-“隐私”-“相机”功能中" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+//        [alert show];
+//        return;
+//    }
     
     AVCaptureMetadataOutput *captureOutput = [[AVCaptureMetadataOutput alloc] init];
     [captureOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    //captureOutput.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
-    [captureOutput setRectOfInterest:CGRectMake(0.35,0.3,0.65,0.7)];//扫码范围，右上角为原点
+    [captureOutput setRectOfInterest:CGRectMake(0.2,0.2,0.5,0.6)];//扫码范围，右上角为原点
     
     AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
     self.captureSession = captureSession;
@@ -59,34 +58,37 @@
     if([self.captureSession canAddOutput:captureOutput]){
         [self.captureSession addOutput:captureOutput];
     }
+    captureOutput.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
     self.captureSession.sessionPreset = AVCaptureSessionPresetHigh;//设置扫描的画质
     
     self.captureVideoPreviewLayer =[AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
     self.captureVideoPreviewLayer.frame = self.view.layer.bounds;
     self.captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     [self.view.layer addSublayer:self.captureVideoPreviewLayer];
-    PLog(@"222");
+    NSLog(@"222");
 }
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"Scanning";
-    coverImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 44, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-88)];
-    coverImage.image = [UIImage imageNamed:(@"Sony_TVLogin_bound.png")];
-    
-    UILabel *alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 380, 300, 40)];
-    alertLabel.text = @"Please scanning QR code!";
-    [coverImage addSubview:alertLabel];
-    [self.view addSubview:coverImage];
     [self initCapture];
     [self startScan];
+    
+    self.title = @"Scanning";
+    coverImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 44, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-88)];
+    coverImage.image = [UIImage imageNamed:@"Sony_TVLogin_bound.png"];
+       
+    UILabel *alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, CGRectGetHeight(coverImage.frame)-160, 300, 40)];
+    alertLabel.text = @"Please Scanning QR code";
+    alertLabel.textColor = [UIColor whiteColor];
+    [coverImage addSubview:alertLabel];
+    [self.view addSubview:coverImage];
 }
 
 //循环♻️的绿色扫码条
 -(void)loopDrawLine
 {
-    CGRect  rect = CGRectMake(68/2+30,140, 502/2, 8/2);//扫描开始位置
+    CGRect  rect = CGRectMake(75,180,502/2, 8/2);//扫描开始位置
     if (readLineView) {
         [readLineView removeFromSuperview];
         readLineView = nil;
@@ -98,7 +100,7 @@
                         options:UIViewAnimationOptionCurveLinear
                      animations:^{
                          //修改fream的代码写在这里
-                         readLineView.frame =CGRectMake(68/2+30,380, 502/2, 8/2);//扫描结束位置
+                         readLineView.frame =CGRectMake(75,360, 502/2, 8/2);//扫描结束位置
                          [readLineView setAnimationRepeatCount:0];
                      }
                      completion:^(BOOL finished){
@@ -111,7 +113,7 @@
 -(void)startScan{
     [self.captureSession startRunning];
     [self loopDrawLine];
-    PLog(@"333");
+    NSLog(@"333");
 }
 
 //扫码条停止循环
@@ -134,16 +136,20 @@
         [self stopScan];
         AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
         stringValue = metadataObject.stringValue;//扫描出信息
-        PLog(@"%@",stringValue);
+        NSLog(@"%@",stringValue);
+        
+        //判断扫描出的信息是否正确
+        if(![stringValue isEqualToString:@""])
+        {
+            [self.navigationController popToRootViewControllerAnimated:YES];
+        }else{
+        
+        }
     }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
--(void)dealloc{
-
 }
 @end
